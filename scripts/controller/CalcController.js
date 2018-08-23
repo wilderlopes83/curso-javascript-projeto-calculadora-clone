6aaa290df69d8341884cd08ecd066c38db2db480
Wilder Lopes <WSLopes@ecad.org.br>
2018-08-23 18:10:35 -0300

    Códigos até a aula C09

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController{
 
     constructor (){
+        this._operation = [];
         this._displayCalcEL = document.querySelector("#display");
         this._dateEL = document.querySelector("#data");
         this._timeEL = document.querySelector("#hora");
@@ -24,14 +25,90 @@ class CalcController{
         }, 1000)
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ')        .forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
+    }
+
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    addOperation(value){
+        
+        this._operation.push(value);
+
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+        switch(value){
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry
+                break;
+            case 'soma':
+                
+                break;                
+            case 'subtracao':
+                
+                break;                                
+            case 'multiplicacao':
+                
+                break;                                
+            case 'divisao':
+                
+                break;                                                
+            case 'porcento':
+                
+                break;                                                                
+            case 'igual':
+                
+                break;                                                                                
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+
+        }
+    }
+
     initButtonsEvents(){
         //consulta no dom por todas as classes "g" abaixo do id "buttons" 
         //e todas as classes "g" abaixo do id "parts"
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");        
 
-        buttons.forEach((btn, index) => {
-            btn.addEventListener('click', e=>{
-                console.log(btn.className.baseVal.replace("btn-", ""));
+        buttons.forEach((btn) => {
+            this.addEventListenerAll(btn, 'click drag', () => {
+                //console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', () => {
+                btn.style.cursor = "pointer";
             })
         });
                 
